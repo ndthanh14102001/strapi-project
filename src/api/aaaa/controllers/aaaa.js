de7f2401aff76099d8f370a38a9bce1f86913911
thanh <thanh.nguyen@beetech.com>
2023-03-12 22:07:57 +0700

    test aaa

---
+++ src/api/aaaa/controllers/aaaa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * aaaa controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::aaaa.aaaa');
---
+++ src/api/aaaa/routes/aaaa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * aaaa router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::aaaa.aaaa');
