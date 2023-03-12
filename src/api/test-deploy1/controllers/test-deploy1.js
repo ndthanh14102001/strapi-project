ac17d7506a4fcab35b95bfd5f7f7455ea9cfa4f2
thanh <thanh.nguyen@beetech.com>
2023-03-12 18:43:34 +0700

    Revert "add test"
    This reverts commit f238698f31a8b24eb9144ec8e0017076c0ec8811.

--- src/api/test-deploy1/controllers/test-deploy1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test-deploy1 controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::test-deploy1.test-deploy1');
--- src/api/test-deploy1/services/test-deploy1.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * test-deploy1 service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::test-deploy1.test-deploy1');
